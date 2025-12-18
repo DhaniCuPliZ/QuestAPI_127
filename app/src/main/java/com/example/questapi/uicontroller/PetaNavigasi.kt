2bc64e3867ef4c79f31ffcb3ecbbd44d92dcf312
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-18 18:37:21 +0700

    Menambahkan navigasi aplikasi data siswa

---
+++ app/src/main/java/com/example/questapi/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,40 @@
+package com.example.questapi.uicontroller
+
+
+import com.example.questapi.uicontroller.route.DestinasiEntry
+import com.example.questapi.uicontroller.route.DestinasiHome
+import com.example.questapi.view.EntrySiswaScreen
+import com.example.questapi.view.HomeScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+
+    }
+}

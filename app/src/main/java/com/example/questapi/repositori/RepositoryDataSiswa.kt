cd596d72bc7277611ce6715e36234bf03487ab7d
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-18 16:33:16 +0700

    menambahkan fungsi ambil dan simpan data siswa

---
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.questapi.repositori
+
+import com.example.questapi.apiservice.ServiceApiSiswa
+import com.example.questapi.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+
+
+}
+

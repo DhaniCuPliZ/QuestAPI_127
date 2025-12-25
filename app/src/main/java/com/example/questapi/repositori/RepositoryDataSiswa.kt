35c68749cd4a32611dc71e4d97887973fadce9be
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-25 23:17:01 +0700

    menambahkan interface edit satu siswa

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -10,6 +10,7 @@ interface RepositoryDataSiswa {
 
     suspend fun getSatuSiswa(id: Int): DataSiswa
 
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
 
 class JaringanRepositoryDataSiswa(

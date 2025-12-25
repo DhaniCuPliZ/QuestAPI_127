9308f48463dd316d4c5bcc4798ff48a82225748c
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-25 23:25:40 +0700

    Menambahkan navigasi ke halaman edit siswa

--- app/src/main/java/com/example/questapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi/uicontroller/PetaNavigasi.kt
@@ -50,6 +50,17 @@ fun HostNavigasi(
                 }
             )
         }
+        composable(
+            DestinasiEdit.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() }
+            )
+        }
 
     }
 }

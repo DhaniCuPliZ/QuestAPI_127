f15d1a7e1d68f02a632583386cb6e3e3a2a23aa2
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-25 23:12:51 +0700

    Menambahkan method Get,Put dan Delete

--- app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
@@ -2,8 +2,11 @@ package com.example.questapi.apiservice
 
 import com.example.questapi.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET(value = "bacaTeman.php")
@@ -12,11 +15,13 @@ interface ServiceApiSiswa {
     @POST(value = "insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 
-    // @GET("baca1Teman.php/{id}")
-    // suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
-    //
-    // @PUT("editTM.php/{id}")
-    // suspend fun editSatuSiswa(@Query("id") id:Int,@Body datasiswa: DataSiswa):ratrofit.Response
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
 
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(@Query("id") id:Int,@Body datasiswa: DataSiswa):retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(@Query("id") id: Int):retrofit2.Response<Void>
 
 }

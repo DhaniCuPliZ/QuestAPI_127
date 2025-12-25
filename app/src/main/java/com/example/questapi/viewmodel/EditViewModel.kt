9d77ac4a8c5c6be1c08149a8a9ec65d375ebc44a
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-25 23:34:08 +0700

    Menambahkan EditViewModel untuk halaman edit siswa

--- app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/EditViewModel.kt
@@ -1,2 +1,41 @@
 package com.example.questapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mydatasiswa.modeldata.DetailSiswa
+import com.example.mydatasiswa.modeldata.UIStateSiswa
+import com.example.mydatasiswa.modeldata.toDataSiswa
+import com.example.mydatasiswa.modeldata.toUiStateSiswa
+import com.example.mydatasiswa.repositori.RepositoryDataSiswa
+import com.example.mydatasiswa.uicontroller.route.DestinasiDetail
+import com.example.questapi.modeldata.DetailSiswa
+import com.example.questapi.modeldata.UIStateSiswa
+import com.example.questapi.modeldata.toDataSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int =
+        checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa
+                .getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
+

12920f3c1411edf7ee5076531d6f54783d32ecc5
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-25 23:24:59 +0700

    Menambahkan navigasi ke halaman detail siswa

--- app/src/main/java/com/example/questapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi/uicontroller/PetaNavigasi.kt
@@ -35,6 +35,21 @@ fun HostNavigasi(
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
         }
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                navigateToEditItem = {
+                    navController.navigate("${DestinasiEdit.route}/${it}")
+                },
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
 
     }
 }

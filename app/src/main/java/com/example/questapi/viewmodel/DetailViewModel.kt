9f14001ed4623869d1a3aa07bad3de408c6281ce
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-12-25 23:40:01 +0700

    Menambahkan StatusUIDetail untuk pengelolaan state UI

--- app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,26 @@
+@file:OptIn(InternalSerializationApi::class)
 package com.example.questapi.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi.modeldata.DataSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import com.example.questapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val datasiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
 
